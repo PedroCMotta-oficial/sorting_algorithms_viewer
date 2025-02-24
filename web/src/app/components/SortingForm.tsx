4d6bd5d8b59fc3e0ae4ee967e1ac8e13b2c48f2e
Pedro Costa da Motta <79551989+PedroCMotta-oficial@users.noreply.github.com>
2025-02-24 15:49:35 -0300

    sorting form v2.0

--- web/src/app/components/SortingForm.tsx
+++ web/src/app/components/SortingForm.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+
 export default function SortingForm() {
+  const [algorithm, setAlgorithm] = useState("bubbleSort");
+
+  const generateRandomArray = (size: number) => {
+    return Array.from({length: size}, () => Math.floor(Math.random()*100) + 1);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const array = generateRandomArray(10);
+
+    console.log("🔹 Formulário enviado!");
+    console.log("📌 Algoritmo escolhido:", algorithm);
+    console.log("📊 Lista gerada:", array);
+  };
+
   return(
-    <form className="p-4 bg-gray-700 rounded-lg">
+    <form onSubmit={handleSubmit} className="p-4 bg-gray-700 rounded-lg">
       <label className="block text-white mb-2">Escolha o algoritmo:</label>
-      <select className="w-full p-2 rounded bg-gray-800 text-white">
+      <select
+        className="w-full p-2 rounded bg-gray-800 text-white"
+        value={algorithm}
+        onChange={(e) => setAlgorithm(e.target.value)}
+      >
         <option value="bubbleSort">Bubble Sort</option>
         <option value="mergeSort">Merge Sort</option>
         <option value="quickSort">Quick Sort</option>
