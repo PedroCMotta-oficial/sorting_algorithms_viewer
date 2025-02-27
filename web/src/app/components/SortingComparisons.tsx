d34e547f1e8d6956efacc574cd937c350a2d9bab
Pedro Costa da Motta <79551989+PedroCMotta-oficial@users.noreply.github.com>
2025-02-27 14:51:47 -0300

    UX/UI v5.3

--- web/src/app/components/SortingComparisons.tsx
+++ web/src/app/components/SortingComparisons.tsx
@@ -5,7 +5,7 @@ export default function SortingComparisons() {
     <div className="flex flex-col gap-3">
       <div className="w-2xs h-24 bg-background rounded-sm border-2 border-textSecundary border-dashed flex flex-row items-center justify-between p-[25px]">
         <div>
-          <h3 className="font-bold text-textPrimary">1    Merge Sort</h3>
+          <h3 className="font-bold text-textPrimary"><span className="text-textHighlight">1 </span>Merge Sort</h3>
           <p className="font-semibold text-textSecundary">0.5s</p>
         </div>
         <div><Trophy className="text-textHighlight size-10"/></div>
@@ -13,7 +13,7 @@ export default function SortingComparisons() {
 
       <div className="w-2xs h-24 bg-background rounded-sm border-2 border-textSecundary border-dashed flex flex-row items-center justify-between p-[25px]">
         <div>
-          <h3 className="font-bold text-textPrimary">2    Radix Sort</h3>
+          <h3 className="font-bold text-textPrimary"><span className="text-textHighlight">2 </span>    Radix Sort</h3>
           <p className="font-semibold text-textSecundary">0.75s</p>
         </div>
         <div>{/*<Trophy className="text-textHighlight size-10"/>*/}</div>
@@ -21,7 +21,7 @@ export default function SortingComparisons() {
 
       <div className="w-2xs h-24 bg-background rounded-sm border-2 border-textHighlight border-dashed flex flex-row items-center justify-between p-[25px]">
         <div>
-          <h3 className="font-bold text-textPrimary">3   Bubble Sort</h3>
+          <h3 className="font-bold text-textPrimary"><span className="text-textHighlight">3 </span>   Bubble Sort</h3>
           <p className="font-semibold text-textSecundary">1s</p>
         </div>
         <div>{/*<Trophy className="text-textHighlight size-10"/>*/}</div>
