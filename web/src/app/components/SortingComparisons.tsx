2fa4b9bf1f1afc35343225beb34aac87cd88b737
Pedro Costa da Motta <79551989+PedroCMotta-oficial@users.noreply.github.com>
2025-02-27 13:52:55 -0300

    UX/UI v5.2

--- web/src/app/components/SortingComparisons.tsx
+++ web/src/app/components/SortingComparisons.tsx
@@ -1,9 +1,31 @@
+import { Trophy } from "lucide-react";
+
 export default function SortingComparisons() {
   return (
     <div className="flex flex-col gap-3">
-      <div className="w-2xs h-24 bg-background rounded-sm border-2 border-textSecundary border-dashed"></div>
-      <div className="w-2xs h-24 bg-background rounded-sm border-2 border-textSecundary border-dashed"></div>
-      <div className="w-2xs h-24 bg-background rounded-sm border-2 border-textSecundary border-dashed"></div>
+      <div className="w-2xs h-24 bg-background rounded-sm border-2 border-textSecundary border-dashed flex flex-row items-center justify-between p-[25px]">
+        <div>
+          <h3 className="font-bold text-textPrimary">1    Merge Sort</h3>
+          <p className="font-semibold text-textSecundary">0.5s</p>
+        </div>
+        <div><Trophy className="text-textHighlight size-10"/></div>
+      </div>
+
+      <div className="w-2xs h-24 bg-background rounded-sm border-2 border-textSecundary border-dashed flex flex-row items-center justify-between p-[25px]">
+        <div>
+          <h3 className="font-bold text-textPrimary">2    Radix Sort</h3>
+          <p className="font-semibold text-textSecundary">0.75s</p>
+        </div>
+        <div>{/*<Trophy className="text-textHighlight size-10"/>*/}</div>
+      </div>
+
+      <div className="w-2xs h-24 bg-background rounded-sm border-2 border-textHighlight border-dashed flex flex-row items-center justify-between p-[25px]">
+        <div>
+          <h3 className="font-bold text-textPrimary">3   Bubble Sort</h3>
+          <p className="font-semibold text-textSecundary">1s</p>
+        </div>
+        <div>{/*<Trophy className="text-textHighlight size-10"/>*/}</div>
+      </div>
     </div>
   )
 }
