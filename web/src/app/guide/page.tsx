6cd94f9246ec0ca6c3de28ff1a9a559a66f7983b
Pedro Costa da Motta <79551989+PedroCMotta-oficial@users.noreply.github.com>
2025-03-06 17:16:50 -0300

    Guide page v2.0

--- web/src/app/guide/page.tsx
+++ web/src/app/guide/page.tsx
@@ -1,19 +1,47 @@
 "use client";
 
+import { ChartNoAxesColumnIncreasing, Github, Clapperboard, PencilRuler, ArrowUpRight } from "lucide-react";
 import { Link, Element} from "react-scroll";
+
 
 export default function Guide() {
   return (
-    <div>
+    <div  className="bg-background min-h-dvh">
+
+      <header>
+        <div className="flex flex-col md:flex-row items-center justify-between px-20 md:px-70 py-4.5">
+
+          <Link href="">
+            <div className="flex flex-row gap-2 items-center cursor-pointer">
+              <ChartNoAxesColumnIncreasing className="size-10 text-textHighlight"/>
+              <h1 className="text-textPrimary font-bold text-2xl">Visualizador de Algoritmos de Ordenação</h1>
+            </div>
+          </Link>
+
+          <div className="flex flex-row gap-10 items-center ">
+
+            <Link href="/guide" className="text-textSecundary font-bold cursor-pointer transition-colors duration-250 hover:text-textHighlight">
+              <div className="p-3">Guia</div>
+            </Link>
+            <a href="https://github.com/PedroCMotta-oficial/sorting_algorithms_viewer" className="text-textSecundary cursor-pointer transition-colors duration-250 hover:text-textHighlight">
+              <div className="p-3"><Github /></div>
+            </a>
+            
+          </div>
+          
+        </div>
+      </header>
+
+
       {/* Sumário */}
-      <nav className="fixed top-0 left-0 bg-white p-4 shadow-md w-full">
+      {/*<nav className="fixed top-0 left-0 bg-white p-4 shadow-md w-full">
         <ul className="flex space-x-4">
           <li><Link to="como-usar" smooth={true} duration={500}>Como Usar</Link></li>
           <li><Link to="significado-cores" smooth={true} duration={500}>Significado das Cores</Link></li>
           <li><Link to="configuracoes" smooth={true} duration={500}>Configurações</Link></li>
           <li><Link to="faq" smooth={true} duration={500}>Dúvidas Comuns</Link></li>
         </ul>
-      </nav>
+      </nav>*/}
 
       {/* Seções */}
       <div className="mt-20 p-4">
@@ -155,7 +183,7 @@ export default function Guide() {
           <p>Explicação de como usar a ferramenta...</p>
           <p>Explicação de como usar a ferramenta...</p>
           <p>Explicação de como usar a ferramenta...</p>
-          
+
         </Element>
 
         <Element name="significado-cores">
