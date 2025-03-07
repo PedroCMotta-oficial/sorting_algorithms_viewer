3db9f51acaae9ff9836dab16e5bc5fdbddf1505a
Pedro Costa da Motta <79551989+PedroCMotta-oficial@users.noreply.github.com>
2025-03-07 12:44:04 -0300

    Guide page v4.0

--- web/src/app/guide/page.tsx
+++ web/src/app/guide/page.tsx
@@ -12,7 +12,7 @@ export default function Guide() {
 
       {/* HEADER */}
       <header>
-        <div className="flex flex-col md:flex-row items-center justify-between px-20 md:px-70 py-4.5">
+        <div className="flex flex-col md:flex-row items-center justify-between px-20 md:px-70 py-4.5 top-0 fixed bg-background w-full">
 
           <Link href="/">
             <div className="flex flex-row gap-2 items-center cursor-pointer">
@@ -33,201 +33,147 @@ export default function Guide() {
           </div>
           
         </div>
+        <div className="h-0.5 w-full  bg-black fixed"></div>
       </header>
 
 
-      <div className="flex flex-row px-75 md:px-[450px] py-4.5 justify-center gap-[50px]">
+      <div className="flex flex-row px-75 md:px-[475px] py-4.5 justify-center gap-[40px] mt-[84px]">
 
         {/* Seções */}
-        <div className="p-4 text-white ">
+        <div className=" text-white w-[700px]">
+
           <Element name="como-usar">
-            <h2>Como Usar</h2>
+            <div>
+              <h1 className="text-textPrimary font-bold text-3xl">Como Usar</h1>
+            </div>
+            
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             
 
           </Element>
 
+          <hr className="text-textSecundary mt-10 mb-10"/>
+
           <Element name="significado-cores">
-            <h2>Significado das Cores</h2>
-            <p>Explicação sobre as cores na animação...</p>
+            <div>
+              <h1 className="text-textPrimary font-bold text-3xl">Significado das Cores</h1>
+            </div>
+
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
           </Element>
+
+          <hr className="text-textSecundary mt-10 mb-10"/>
 
           <Element name="configuracoes">
-            <h2>Configurações</h2>
-            <p>Explicação sobre as opções de personalização...</p>
+            <div>
+              <h1 className="text-textPrimary font-bold text-3xl">Configurações</h1>
+            </div>
+
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
           </Element>
 
+          <hr className="text-textSecundary mt-10 mb-10"/>
+
           <Element name="faq">
-            <h2>Dúvidas Comuns</h2>
-            <p>Respostas para perguntas frequentes...</p>
+            <div>
+              <h1 className="text-textPrimary font-bold text-3xl">Dúvidas Comuns</h1>
+            </div>
+
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
             <p>Explicação de como usar a ferramenta...</p>
             <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
-            <p>Explicação de como usar a ferramenta...</p>
+            <p>Explicação de como usar a ferramenta...Explicação de como usar a ferramenta.. Explicação</p>
           </Element>
+
         </div>
 
         {/* Sumário */}
